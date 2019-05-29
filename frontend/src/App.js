4b8bd3095c53457d69a263fe7dd2284d1b8a3457
Elaine Mattos <elaine.mattos@outlook.pt>
2019-05-28 21:43:55 -0300

    Adjustments (#7)
    * add transition when text gets smaller
    
    * adjustments
    
    * add clouds
    
    * opacity clouds
    descript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Getdata from './components/GetData'
+import Clouds from './components/Clouds.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Clouds />
       <Getdata />
       </div>
     );
---
+++ frontend/src/components/Clouds.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Css/Clouds.css'
+
+const Clouds = () => {
+  return (
+    <div>
+      <div id="background-wrap">
+          <div class="x1">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x2">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x3">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x4">
+              <div class="cloud"></div>
+          </div>
+
+          <div class="x5">
+              <div class="cloud"></div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Clouds
